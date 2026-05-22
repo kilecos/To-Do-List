87cdde1e8d8556337821fc0b7218459effc21674
Kilian Lecossois <kikilecossois@gmail.com>
2026-05-22 14:03:52 +0200

    docs: ajout de commentaires pour importExport.ts

--- src/scripts/importExport.ts
+++ src/scripts/importExport.ts
@@ -1,80 +1,93 @@
+// Gestion de la fonctionnalité d'import/export
+
+// Récupération des types, variables et fonctions depuis les autres fichiers
 import { afficherTaches, titre } from "./ui";
 import { listeTaches, sauvegarderTaches } from "./storage";
 import type { Tache } from "./types";
 
-const importExportModal = document.querySelector("#modal-import-export") as HTMLDialogElement;
-const firstModal = document.querySelector(".modal-first-content") as HTMLDivElement;
-const exportModal = document.querySelector(".modal-export-content") as HTMLDivElement;
-const importModal = document.querySelector(".modal-import-content") as HTMLDivElement;
-const btnOpenModal = document.querySelector("#btn-import-export") as HTMLButtonElement;
-const btnExport = document.querySelector(".btn-export") as HTMLButtonElement;
-const btnImport = document.querySelector(".btn-import") as HTMLButtonElement;
-const btnCancel = document.querySelector(".btn-cancel") as HTMLButtonElement;
-const btnBack = document.querySelectorAll(".btn-back") as NodeListOf<HTMLButtonElement>;
-const btnJson = document.querySelector(".btn-json") as HTMLButtonElement;
-const btnText = document.querySelector(".btn-texte") as HTMLButtonElement;
-const importInput = document.querySelector("#import-input") as HTMLInputElement;
-const btnConfirmImport = document.querySelector(".btn-confirm") as HTMLButtonElement;
+// Définition des constantes à partir des éléments HTML qui vont être utilisés
+const importExportModal = document.querySelector("#modal-import-export") as HTMLDialogElement;   // La fenêtre modale qui s'affiche lors du clic sur le bouton correspondant
+const firstModal = document.querySelector(".modal-first-content") as HTMLDivElement;             // Le contenu initial de la modale proposant l'import ou l'export
+const exportModal = document.querySelector(".modal-export-content") as HTMLDivElement;           // Le contenu de la modale après sélection de l'export
+const importModal = document.querySelector(".modal-import-content") as HTMLDivElement;           // Le contenu de la modale après sélection de l'import
+const btnOpenModal = document.querySelector("#btn-import-export") as HTMLButtonElement;          // Le bouton servant à ouvrir la fenêtre modale
+const btnExport = document.querySelector(".btn-export") as HTMLButtonElement;                    // Le bouton de sélection de l'option export
+const btnImport = document.querySelector(".btn-import") as HTMLButtonElement;                    // Le bouton de sélection de l'option import
+const btnCancel = document.querySelector(".btn-cancel") as HTMLButtonElement;                    // Le bouton fermant la modale
+const btnBack = document.querySelectorAll(".btn-back") as NodeListOf<HTMLButtonElement>;         // Les boutons pour retourner au premier contenu de la modale
+const btnJson = document.querySelector(".btn-json") as HTMLButtonElement;                        // Le bouton de sélection d'export d'un format JSON
+const btnText = document.querySelector(".btn-texte") as HTMLButtonElement;                       // Le bouton de sélection d'export d'un format txt
+const importInput = document.querySelector("#import-input") as HTMLInputElement;                 // L'input qui permet de sélectionner un fichier JSON à importer
+const btnConfirmImport = document.querySelector(".btn-confirm") as HTMLButtonElement;            // Le bouton de lancement de l'import d'un fichier JSON
 
+// Définition de la fonction pour afficher les différents état de la fenêtre modale pour l'import/export
+// Le paramètre etat correspondra au contenu de la modale à afficher
 function afficherEtat(etat : HTMLDivElement) {
+    // On cible d'abord tous les contenu possible de la modale et on retire à chacun la classe visible pour masquer ce contenu
     document.querySelectorAll(".modal-ie-content").forEach((modal) => {
         modal.classList.remove("visible");
     });
+    // On ajoute ensuite la classe visible à notre contenu passer en paramètre pour n'afficher que celui-ci
     etat.classList.add("visible");
 }
 
+// Définition de la fonction contenant tous les écouteurs d'évènement pour afficher la modale, dire quel contenu afficher et lancer les fonction d'import et d'export
+// On exporte cette fonction dans main.ts pour quelle se lance et activer les écouteurs d'évènement dès le chargement de la page
 export function initialiserImportExport() {
+    // Affichage de la modale avec son état initial
     btnOpenModal.addEventListener("click", () => {
         importExportModal.showModal();
         afficherEtat(firstModal);
     });
-
+    // Affichage du contenu proposant les options d'export
     btnExport.addEventListener("click", () => {
         afficherEtat(exportModal);
     });
-
+    // Affichage du contenu de l'interface d'import
     btnImport.addEventListener("click", () => {
         afficherEtat(importModal);
     });
-
+    // Fermeture de la modale par click sur le bouton "Annuler"
     btnCancel.addEventListener("click", () => {
         importExportModal.close();
     });
-
+    // Retour à l'affichage du premier contenu de la modale par clic sur les boutons "Retour"
     btnBack.forEach((btn) => {
         btn.addEventListener("click", () => {
             afficherEtat(firstModal);
         });
     });
-
+    // Fermeture de la modale par clic en dehors de celle-ci
     importExportModal.addEventListener("click", (e) => {
         if (e.target === importExportModal) {
             importExportModal.close();
         }
     });
-
+    // Lancement de la fonction d'exportation d'un fichier JSON contenant notre liste de tâches + fermeture de la modale
     btnJson.addEventListener("click", () => {
         exporterJSON();
         importExportModal.close();
     });
-
+    // Lancement de la fonction d'exportation d'un fichier texte contenant notre liste de tâches + fermeture de la modale
     btnText.addEventListener("click", () => {
         exporterTexte();
         importExportModal.close();
     });
-
+    // Lancement de la fonction d'importation d'un fichier JSON contenant une liste de tâches une fois celui-ci sélectionné dans l'input
     btnConfirmImport.addEventListener("click", () => {
         importerListe();
     });
 }
 
+// Définition de la fonction d'exportation de la liste de tâches sous forme d'un fichier JSON
 function exporterJSON() {
-    // On converti notre tableau listeTaches en chaine de caractères texte au format JSON
-    // null, 2 formate avec des indentations
+    // On défini on objet contenu prenant en paramètre le titre de la liste ainsi que le tableau liste de tâches
     const contenu = {
         titre : titre.textContent,
         liste : listeTaches
     };
+    // On converti notre objet contenu en une unique chaine de caractères texte au format JSON
+    // null, 2 formate avec des indentations
     const contenuExport = JSON.stringify(contenu, null, 2);
     // Un Blob est un objet qui représente des données binaires, c'est le format utilisé par le navigateur pour manipuler des fichiers en mémoire.
     // On lui donne le contenu et lui dit de quel type de fichier il s'agit
@@ -91,12 +104,14 @@ function exporterJSON() {
     URL.revokeObjectURL(url);
 }
 
+// Définition de la fonction d'exportation de la liste de tâches sous forme d'un fichier texte
 function exporterTexte() {
     // On parcours chaque élément de notre tableau listeTaches et retourne un nouveau tableau constitué de chaines de caractères
-    // Avec .join("\n") on transforme ce tableau en une seule chaine de caractères avec des sauts de ligne entre chaque tâche
     const contenu = listeTaches.map((tache) => {
         return `- ${tache.titre}${tache.priorite? ` | [Priorité : ${tache.priorite}]` : ""}${tache.estTerminee? " | ✔" : ""}`;
     });
+    // On intègre également le titre qui va être exporté avec la liste
+    // Avec .join("\n") on transforme le tableau contenu en une seule chaine de caractères avec des sauts de ligne entre chaque tâche
     const contenuExport = `${titre.textContent}\n\n` + contenu.join("\n");
     const blob = new Blob([contenuExport], {type: "text/plain"});
     const url = URL.createObjectURL(blob);
@@ -107,6 +122,7 @@ function exporterTexte() {
     URL.revokeObjectURL(url);
 }
 
+// Définition de la fonction d'importation d'un fichier JSON contenant une liste de tâches
 function importerListe() {
     // On récupère le fichier sélectionné par l'utilisateur via l'input
     const fichier = importInput.files?.[0];
@@ -135,7 +151,9 @@ function importerListe() {
             }
             // On vide la liste de tâches actuelle sur l'application
             listeTaches.length = 0;
+            // On modifie le titre actuel de la liste par le titre de la liste importée si ce dernier existe
             titre.textContent = listeImportee.titre ?? "✔ To-Do List";
+            // On sauvegarde dans le localStorage le titre importé
             localStorage.setItem("mon-titre", titre.textContent ?? "✔ To-Do List");
             // On rempli la liste avec le tableau des données importées
             listeTaches.push(...listeImportee.liste);
