ac617264bef92c2f0b31a44d9708dbfc0ccd1a84
Kilian Lecossois <kikilecossois@gmail.com>
2026-05-28 14:52:44 +0200

    test: modifications description it

--- src/__tests__/importExport.test.ts
+++ src/__tests__/importExport.test.ts
@@ -4,28 +4,28 @@ import { validerDonneesImport } from "../scripts/importExport";
 import type { NiveauPriorite } from "../scripts/types";
 
 describe("validerDonneesImport", () => {
-    it("la fonction retourne false si les donnees ne sont pas un objet", () => {
+    it("Retourne false si les donnees ne sont pas un objet", () => {
         expect(validerDonneesImport("une string")).toBe(false);
     });
-    it("la fonction retourne false si les données sont null", () => {
+    it("Retourne false si les données sont null", () => {
         expect(validerDonneesImport(null)).toBe(false);
     });
-    it("la fonction retourne false si les données importées ne sont pas un tableau", () => {
+    it("Retourne false si les données importées ne sont pas un tableau", () => {
         expect(validerDonneesImport({liste: "pas un tableau"})).toBe(false);
     });
-    it("retourne false si le titre n'est pas une string", () => {
+    it("Retourne false si le titre n'est pas une string", () => {
         expect(validerDonneesImport({liste: [{id: "string", titre: 1234, estTerminee: false}]})).toBe(false);
     });
-    it("retourne false si estTerminee n'est pas de type boolean", () => {
+    it("Retourne false si estTerminee n'est pas de type boolean", () => {
         expect(validerDonneesImport({liste: [{id: "string", titre: "string", estTerminee: "string"}]})).toBe(false);
     });
-    it("retourne false si id n'est pas une string", () => {
+    it("Retourne false si id n'est pas une string", () => {
         expect(validerDonneesImport({liste: [{id: 1234, titre: "string", estTerminee: true}]})).toBe(false);
     });
-    it("retourne false si une des propriété est manquante", () => {
+    it("Retourne false si une des propriété est manquante", () => {
         expect(validerDonneesImport({liste: [{titre: "string", estTerminee: false}]})).toBe(false);
     });
-    it("retourne true si les données importées passent les validations", () => {
+    it("Retourne true si les données importées passent les validations", () => {
         expect(validerDonneesImport({
             titre: "Ma liste test",
             liste: [{id: "string", titre: "string", estTerminee: false, priorite: "" as NiveauPriorite}]
