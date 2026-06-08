0ca6492c111e908ea22f20c53fc5d6c2b381135c
Kilian Lecossois <kikilecossois@gmail.com>
2026-06-08 14:23:17 +0200

    test: ajout test unitaire fonction modeEdition de ui.ts

--- src/__tests__/ui.test.ts
+++ src/__tests__/ui.test.ts
@@ -227,4 +227,13 @@ describe("modeEdition", () => {
         expect(input).not.toBeNull();
         expect(input.value).toBe("Faire les courses");
     });
+    it("un select a bien été créé", () => {
+       const tache : Tache = { id: "1", titre: "Faire les courses", estTerminee: false, priorite: "Haute"};
+        const li = document.createElement("li");
+        listeTachesHtml.appendChild(li);
+        modeEdition(tache, li);
+        const select = li.querySelector("select") as HTMLSelectElement;
+        expect(select).not.toBeNull();
+        expect(select.value).toBe("Haute"); 
+    });
 });
