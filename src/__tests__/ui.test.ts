190663576b2e7c862d4c918c63755ec1ea959b1d
Kilian Lecossois <kikilecossois@gmail.com>
2026-06-08 14:33:20 +0200

    test: ré écriture test fonction modeEdition de ui.ts

--- src/__tests__/ui.test.ts
+++ src/__tests__/ui.test.ts
@@ -211,36 +211,30 @@ describe("miseAJourCompteur", () => {
 });
 
 describe("modeEdition", () => {
-    it("la classe en-edition est bien ajoutée au li", () => {
-        const tache : Tache = { id: "1", titre: "Faire les courses", estTerminee: false, priorite: "Haute"};
-        const li = document.createElement("li");
+    let tache: Tache;
+    let li: HTMLLIElement;
+
+    beforeEach(() => {
+        tache = { id: "1", titre: "Faire les courses", estTerminee: false, priorite: "Haute"};
+        li = document.createElement("li");
         listeTachesHtml.appendChild(li);
         modeEdition(tache, li);
+    });
+
+    it("la classe en-edition est bien ajoutée au li", () => {
         expect(li.classList.contains("en-edition")).toBe(true);
     });
     it("un input a bien été créé", () => {
-        const tache : Tache = { id: "1", titre: "Faire les courses", estTerminee: false, priorite: "Haute"};
-        const li = document.createElement("li");
-        listeTachesHtml.appendChild(li);
-        modeEdition(tache, li);
         const input = li.querySelector("input") as HTMLInputElement;
         expect(input).not.toBeNull();
         expect(input.value).toBe("Faire les courses");
     });
     it("un select a bien été créé", () => {
-        const tache : Tache = { id: "1", titre: "Faire les courses", estTerminee: false, priorite: "Haute"};
-        const li = document.createElement("li");
-        listeTachesHtml.appendChild(li);
-        modeEdition(tache, li);
         const select = li.querySelector("select") as HTMLSelectElement;
         expect(select).not.toBeNull();
         expect(select.value).toBe("Haute"); 
     });
     it("le bouton Editer a bien été créé", () => {
-        const tache : Tache = { id: "1", titre: "Faire les courses", estTerminee: false, priorite: "Haute"};
-        const li = document.createElement("li");
-        listeTachesHtml.appendChild(li);
-        modeEdition(tache, li);
         const editer = li.querySelector("button") as HTMLButtonElement;
         expect(editer).not.toBeNull();
     });
